e91d5447ddf77c3611592c0670ab7bb8f8f63c81
cameronindetroit <cameronindetroit@gmail.com>
2018-07-25 22:43:54 -0400

    Notaded Code

--- ShoppingList.java
+++ ShoppingList.java
@@ -19,40 +19,50 @@ public class ShoppingList {
 		// Class variables
 		String prompt;
 		String newItem;
-		ArrayList<String> shoppingCart = new ArrayList<String>(); // shopping cart
-		HashMap<String, Double> inventory = new HashMap<>(); // inventory
-
 		String yesOrNo = "yes";
 
+		// Class ArrayList
+		ArrayList<String> shoppingCart = new ArrayList<String>(); // shopping cart
+
+		// Class Map
+		HashMap<String, Double> inventory = new HashMap<>(); // inventory
+
+		// Fill inventory using method
 		inventory = ShoppingListMethods.fillInventory(inventory);
-		// displays the inventory
+
+		// displays the inventory using method
 		ShoppingListMethods.displayInventory(inventory);
 
 		do {
 			// Enter item name
 			prompt = "What would you like to order?";
-			// fills the Inventory
 			newItem = ShoppingListMethods.enterItem(scnr, inventory, prompt);
 
-			// Conditional to make sure valid item is being added to shopping cart
-
+			// Add item to shopping cart
 			shoppingCart.add(newItem);
 
+			// Line for spacing
 			System.out.println();
 
-			// System.out.println(shoppingCart);
-
+			// Continue Adding items
 			yesOrNo = Validator.YesOrNo(scnr, "Would you like to order anything else (yes or no)?");
 
 		} while (yesOrNo.equals("yes"));
 
+		// Ending statements
 		System.out.println("Thanks for your order!");
 		System.out.println("Here's what you got: ");
+
+		// Display items added to shopping cart
 		ShoppingListMethods.displayShoppingCart(shoppingCart, inventory);
 
+		// Display average of all items in cart
 		ShoppingListMethods.average(shoppingCart, inventory);
 
+		// Display price of most expensive item
 		ShoppingListMethods.highest(shoppingCart, inventory);
+
+		// Display price of least expensive item
 		ShoppingListMethods.lowest(shoppingCart, inventory);
 
 	}
